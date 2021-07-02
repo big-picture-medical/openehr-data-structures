62a3f8d8fd0cc43ec1c0236e385ff55eaf6905a1
Jess Archer <jess@jessarcher.com>
2021-07-02 14:19:37 +1000

    Add PROPORTION_KIND constants to DV_PROPORTION

--- src/Rm/DataTypes/Quantity/DvProportion.php
+++ src/Rm/DataTypes/Quantity/DvProportion.php
@@ -4,6 +4,16 @@ namespace BigPictureMedical\OpenEhr\Rm\DataTypes\Quantity;
 
 class DvProportion extends DvAmount
 {
+    public const PK_RATIO = 0;
+
+    public const PK_UNITARY = 1;
+
+    public const PK_PERCENT = 2;
+
+    public const PK_FRACTION = 3;
+
+    public const PK_INTEGER_FRACTION = 4;
+
     public string $_type = 'DV_PROPORTION';
 
     public float $numerator;
