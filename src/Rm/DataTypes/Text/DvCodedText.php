91c8b33e05891373f4f59e5b97275427f5965eea
Jess Archer <jess@jessarcher.com>
2021-07-09 15:27:21 +1000

    Add make method to DvCodedText

--- src/Rm/DataTypes/Text/DvCodedText.php
+++ src/Rm/DataTypes/Text/DvCodedText.php
@@ -2,9 +2,24 @@
 
 namespace BigPictureMedical\OpenEhr\Rm\DataTypes\Text;
 
+use BigPictureMedical\OpenEhr\Base\BaseTypes\Identification\TerminologyId;
+
 class DvCodedText extends DvText
 {
     public string $_type = 'DV_CODED_TEXT';
 
     public CodePhrase $defining_code;
+
+    public static function make(string $value, string $codeString, string $terminologyId): self
+    {
+        return new self([
+            'value' => $value,
+            'defining_code' => new CodePhrase([
+                'code_string' => $codeString,
+                'terminology_id' => new TerminologyId([
+                    'value' => $terminologyId,
+                ]),
+            ]),
+        ]);
+    }
 }
